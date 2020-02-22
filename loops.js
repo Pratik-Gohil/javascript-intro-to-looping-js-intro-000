9790f4b7f484df399d3610c4cb5f43d3ba0307c3
Pratik <pr.tikgohil@gmail.com>
2020-02-22 18:25:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 1; i < 26; i++) {
-    array.push(`I am ${i} strange loop${i < 2 ? '' : 's'}.`)
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
   }
   return array
 }
